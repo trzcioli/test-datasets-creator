1fee77c1d6b034d405886ba95ae9d657ca4084c4
trzcioli <trzcioli@gmail.com>
2019-05-20 21:50:02 +0200

    add testing if class_file contain rows with non ascii name after load

--- tests/test_load_files.py
+++ tests/test_load_files.py
@@ -1,5 +1,5 @@
 import unittest
-from app.load_files import load_file
+from app.load_files import load_file, modify_class_file
 import os
 
 path = os.path.dirname(os.path.dirname(__file__))
@@ -24,6 +24,14 @@ class TestLoadFiles(unittest.TestCase):
 
     def test_load_class_file_num_of_rows(self):
         self.assertEqual(615, load_file('class', path).shape[0])
+
+    def test_if_class_file_correct_without_slash(self):
+        class_file = modify_class_file(path)
+        self.assertFalse(32 in class_file.id.values)
+
+    def test_if_class_file_correct_without_non_ascii_signs(self):
+        class_file = modify_class_file(path)
+        self.assertFalse(621 in class_file.id.values)
 
 
 if __name__ == '__main__':
