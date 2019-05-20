3bc5acf5b6cbc6471ba29ed2749a93b30cf81c06
trzcioli <trzcioli@gmail.com>
2019-05-20 21:46:39 +0200

    add testing num of rows after load_files

--- tests/test_load_files.py
+++ tests/test_load_files.py
@@ -16,6 +16,15 @@ class TestLoadFiles(unittest.TestCase):
     def test_load_class_file_num_of_cols(self):
         self.assertEqual(9, load_file('class', path).shape[1])
 
+    def test_load_test_level_file_num_of_rows(self):
+        self.assertEqual(5, load_file('test_level', path).shape[0])
+
+    def test_load_test_file_num_of_rows(self):
+        self.assertEqual(13039, load_file('test', path).shape[0])
+
+    def test_load_class_file_num_of_rows(self):
+        self.assertEqual(615, load_file('class', path).shape[0])
+
 
 if __name__ == '__main__':
     unittest.main()
