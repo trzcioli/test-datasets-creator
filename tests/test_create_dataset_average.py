48dd9a5294cf9a198743b3287fd9fb55ff773cd0
trzcioli <trzcioli@gmail.com>
2019-05-20 22:47:29 +0200

    add test of count_avg_class_test_overall_score

--- tests/test_create_dataset_average.py
+++ tests/test_create_dataset_average.py
@@ -1,7 +1,9 @@
 import unittest
-from app.create_dataset_average import filter_student_with_scored_test, filter_test_status_scoring_scored
+from app.create_dataset_average import filter_student_with_scored_test, filter_test_status_scoring_scored, \
+    group_by_class_and_mean_of_overall_score
 import os
 import pandas as pd
+import numpy as np
 
 path = os.path.dirname(os.path.dirname(__file__))
 
@@ -15,6 +17,15 @@ class TestCreateDatasetAverage(unittest.TestCase):
     def test_filter_test_status_scoring_scored(self):
         test_file = pd.read_csv(path + '/app/files/test_file_load.csv')
         self.assertFalse(((filter_test_status_scoring_scored(test_file))['test_status'] != 'SCORING_SCORED').any())
+
+    def test_count_avg_class_test_overall_score(self):
+        test_file = pd.read_csv(path + '/app/files/test_file_load.csv')
+        only_one_class = test_file.loc[test_file['class_id'] == 5]
+        only_with_overall_score = only_one_class.dropna(subset=['overall_score'])
+        avg_expected = np.asarray(only_with_overall_score.overall_score.values, dtype=float).mean()
+        calc_mean = group_by_class_and_mean_of_overall_score(only_with_overall_score)
+        avg_actual = calc_mean.avg_class_test_overall_score
+        self.assertEqual(avg_expected, float(avg_actual))
 
 
 if __name__ == '__main__':
