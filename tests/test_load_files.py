bf1776cc88b6cef7b8b8b63a662d377403847899
trzcioli <trzcioli@gmail.com>
2019-05-20 21:44:59 +0200

    add testing num of cols after load

---
+++ tests/test_load_files.py
@@ -0,0 +1,21 @@
+import unittest
+from app.load_files import load_file
+import os
+
+path = os.path.dirname(os.path.dirname(__file__))
+
+
+class TestLoadFiles(unittest.TestCase):
+
+    def test_load_test_level_file_num_of_cols(self):
+        self.assertEqual(5, load_file('test_level', path).shape[1])
+
+    def test_load_test_file_num_of_cols(self):
+        self.assertEqual(17, load_file('test', path).shape[1])
+
+    def test_load_class_file_num_of_cols(self):
+        self.assertEqual(9, load_file('class', path).shape[1])
+
+
+if __name__ == '__main__':
+    unittest.main()
