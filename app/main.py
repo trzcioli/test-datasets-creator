2e75e96a63f8ab2064c39f548937a3be243c744b
trzcioli <trzcioli@gmail.com>
2019-05-21 20:39:16 +0200

    add argparse to can execute only one script

--- app/main.py
+++ app/main.py
@@ -3,14 +3,32 @@ from create_dataset_utilization import create_dataset_utilization
 from create_dataset_average import create_dataset_average
 from load_datasets_to_database import load_datasets_to_database
 import os
+import argparse
 
 
 def main():
     path = '/'.join(os.path.abspath(__file__).split('/')[:-2])
-    load_files(path)
-    create_dataset_utilization(path)
-    create_dataset_average(path)
-    load_datasets_to_database()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-lf", "--load_files", help="load csv files from input_files",  action='store_true')
+    parser.add_argument("-cuti", "--create_dataset_utilization", help="create dataset utilization",  action='store_true')
+    parser.add_argument("-cavg", "--create_dataset_average", help="create dataset average",  action='store_true')
+    parser.add_argument("-ld", "--load_datasets_to_database", help="load datasets to database",  action='store_true')
+    args = parser.parse_args()
+
+    if not any(vars(args).values()):
+        load_files(path)
+        create_dataset_utilization(path)
+        create_dataset_average(path)
+        load_datasets_to_database()
+    elif args.load_files:
+        load_files(path)
+    elif args.create_dataset_utilization:
+        create_dataset_utilization(path)
+    elif args.create_dataset_average:
+        create_dataset_average(path)
+    elif args.load_datasets_to_database:
+        load_datasets_to_database()
 
 
 if __name__ == '__main__':
